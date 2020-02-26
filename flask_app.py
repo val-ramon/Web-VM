c36e7c0b60647103b7aa18b234ed52e0290dd435
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-26 15:46:13 -0300

    Reformulación del código para mejor entendimiento

--- flask_app.py
+++ flask_app.py
@@ -9,7 +9,11 @@ from flask import render_template, request, redirect, url_for
 from flask import Flask
 from bokeh.embed import components
 from bokeh.layouts import row, layout
-from bokeh_figure import *
+from bokeh_map import *
+from bokeh_system_state import *
+from bokeh_historics import *
+from bokeh_waterfall import *
+from bokeh_user_act import *
 import time
 import threading
 import os
@@ -17,9 +21,7 @@ import os
 
 app = Flask(__name__)
 contActivos = 0
-#socketio = SocketIO(app)
-cant_zonas = [5, 10, 20]
-feature_names = [str(x) for x in cant_zonas]
+feature_names = ['15']
 ips = {}
 
 
@@ -41,10 +43,10 @@ def elimina_ips():
                         print ('Usuario ' + key + ' inactivo durante mas de 5 minutos. Desconectado.')
                         contActivos = 0
                         break
-#                        return render_template("logout.html")
         except:
             pass
 
+# Index page
 @app.route('/')
 def home():
     """
@@ -60,7 +62,7 @@ def home():
     if contActivos == 0 or ip in ips.keys():
         ips[ip] = time.time()
         contActivos = 1
-        return redirect(url_for('page'))
+        return redirect(url_for('map_zones'))
     else:
         return redirect(url_for('user_act'))
 
@@ -90,14 +92,13 @@ def user_act():
     wfActivo = 0
     # Determine the selected feature
     div = usuario_activo()
-#    layout = column(p, row(select, column(div,text_input_eventos, text_input_intensidad), column(button, button2, boton_elimina_zona), checkbox_group), boton_agregar_zona, column(row(text_input_prog_ini, text_input_prog_fin), boton_confima_zona))  
     layout_b = layout(div)
     # Embed plot into HTML via Flask Render
     script, div = components(layout_b)
     return render_template("user_act.html")
 
 @app.route('/wf')
-def my_waterfall():
+def waterfall():
     """
         Muestra al usuario el waterfall, además de actualizar el contador de tiempo, ya que existe un usuario
         que está realizando cambios. En caso de no haber alguien, se le asigna un tiempo inicial.
@@ -115,7 +116,7 @@ def my_waterfall():
     return render_template("waterfall.html", script=script, div=div)
 
 @app.route('/state')
-def my_state():
+def system_state():
     """
         Muestra al usuario el estado del equipo, además de actualizar el contador de tiempo, ya que existe un usuario
         que está realizando cambios. En caso de no haber alguien, se le asigna un tiempo inicial.
@@ -128,14 +129,12 @@ def my_state():
         ips[ip] = time.time()
     else:
         ips[ip] = time.time()
-#    divTemp, divTens, divSnr, temp, tens, snr, divC, divV = visualiza_estado()
     p, p2 = visualiza_estado()
-#    script, div = components(layout(row(layout(divTemp, divTens, divSnr), layout(temp, tens, snr), layout(divC, divV))))
     script, div = components(row(p, p2))
     return render_template("estado.html", script=script, div=div)
 
 @app.route('/table')
-def my_page_of_data_table():
+def table_historics():
     """
         Muestra al usuario la tabla de históricos, además de actualizar el contador de tiempo, ya que existe un usuario
         que está realizando cambios. En caso de no haber alguien, se le asigna un tiempo inicial.
@@ -148,13 +147,13 @@ def my_page_of_data_table():
         ips[ip] = time.time()
     else:
         ips[ip] = time.time()
-    data_table, boton_vuelve, range_slider, checkbox_eventos, div, checkbox_estado, div2, filtro_id, boton_sube, boton_baja, filtro_fecha_ini, filtro_fecha_fin, boton_filtra_fecha = visualiza_historicos()
+    data_table, range_slider, checkbox_eventos, div, checkbox_estado, div2, filtro_id, boton_sube, boton_baja, filtro_fecha_ini, filtro_fecha_fin, boton_filtra_fecha = visualiza_historicos()
     script, div = components(layout(row(layout(data_table, boton_sube, boton_baja), layout(range_slider, div, checkbox_eventos, div2, checkbox_estado, filtro_id, filtro_fecha_ini, filtro_fecha_fin, boton_filtra_fecha))))
     return render_template("historicos.html", script=script, div=div)
 
-# Index page
+
 @app.route('/map')
-def page():
+def map_zones():
     """
         Muestra al usuario el mapa, además de actualizar el contador de tiempo, ya que existe un usuario
         que está realizando cambios. En caso de no haber alguien, se le asigna un tiempo inicial.
@@ -165,16 +164,15 @@ def page():
     global contActivos, ips, wfActivo
     wfActivo = 0
     ip = request.remote_addr
-    pathAutorizados = os.environ['USERPROFILE']+'/Downloads/bokeh_flask/bokeh_flask/usuariosAutorizados.txt'
+    pathAutorizados = 'C:/Users/edomene/Downloads/bokeh_flask/bokeh_flask/usuariosAutorizados.txt'
     fileO = open(pathAutorizados, 'r')
     obj = fileO.read()
     fileO.close()
     usuariosHabilitados = obj.split('\n')
-#    if ip not in usuariosHabilitados:
-#        usuarioAutorizado = 0
-#    else:
-#        usuarioAutorizado = 1
-    usuarioAutorizado = 1
+    if ip in usuariosHabilitados:
+        usuarioAutorizado = 0
+    else:
+        usuarioAutorizado = 1
     if ip not in ips.keys() and contActivos == 0:
         ips[ip] = time.time()
         contActivos = 1
@@ -185,11 +183,7 @@ def page():
                 current_feature_name = "15"
     
         # Create the plot
-    #    p, checkbox_group, select, text_input_eventos, text_input_intensidad, button, button2, div, boton_agregar_zona, text_input_prog_ini, text_input_prog_fin, boton_confima_zona, boton_elimina_zona = create_figure(current_feature_name)
-        p, checkbox_group, select, input_eventos, slider_intensidad, button, button2, div, div2, data_table, boton_muestra_eventos_zona, hover_t = create_figure(current_feature_name)
-    #    layout = column(p, row(select, column(div,text_input_eventos, text_input_intensidad), column(button, button2, boton_elimina_zona), checkbox_group), boton_agregar_zona, column(row(text_input_prog_ini, text_input_prog_fin), boton_confima_zona))  
-    #    layout_b = layout(p, row(select, layout(div,text_input_eventos, text_input_intensidad), layout(button, button2, boton_elimina_zona), checkbox_group), boton_agregar_zona, layout(row(text_input_prog_ini, text_input_prog_fin), boton_confima_zona))
-    #    buton_muestra_todos_eventos.on_click = my_page_of_data_table()
+        p, checkbox_group, select, input_eventos, slider_intensidad, button, button2, div, div2, data_table, boton_muestra_eventos_zona, hover_t = create_map(current_feature_name)
         p.add_tools(hover_t)
         layout_b = layout(row(p, layout(div2, data_table)), row(select, layout(div,input_eventos, slider_intensidad), layout(button, boton_muestra_eventos_zona, button2), checkbox_group))
         # Embed plot into HTML via Flask Render
@@ -202,8 +196,7 @@ def page():
                 current_feature_name = "15"
     
         # Create the plot
-    #    p, checkbox_group, select, text_input_eventos, text_input_intensidad, button, button2, div, boton_agregar_zona, text_input_prog_ini, text_input_prog_fin, boton_confima_zona, boton_elimina_zona = create_figure(current_feature_name)
-        p, checkbox_group, select, input_eventos, slider_intensidad, button, button2, div, div2, data_table, boton_muestra_eventos_zona, hover_t = create_figure(current_feature_name)
+        p, checkbox_group, select, input_eventos, slider_intensidad, button, button2, div, div2, data_table, boton_muestra_eventos_zona, hover_t = create_map(current_feature_name)
         p.tools = []
         p.add_tools(hover_t)
         checkbox_group.disabled = True
@@ -232,8 +225,7 @@ if __name__ == '__main__':
     thread_elimina_ips = threading.Thread(target=elimina_ips)
     thread_elimina_ips.setDaemon(True)
     thread_elimina_ips.start()
+    app.secret_key = os.urandom(12)
     os.system("start cmd /k mueve_archivo_cambios.py")
     os.system("start cmd /k ejemplo_select.py")
-    app.secret_key = os.urandom(12)
-    app.run(port=5006, host='0.0.0.0')
-#    socketio.run(app, port=5000, host='0.0.0.0')
+    app.run(port=5000, host='0.0.0.0')
