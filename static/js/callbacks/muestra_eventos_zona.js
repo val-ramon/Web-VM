dd4ca243f8a8b0b4749223ed25505e61e853a0af
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 09:33:52 -0300

    Agregado de comentarios

--- static/js/callbacks/muestra_eventos_zona.js
+++ static/js/callbacks/muestra_eventos_zona.js
@@ -1,13 +1,23 @@
-var label = cb_obj['label'];
-var nueva_data = select.value;
-var indice = source.data.Intensity_tip.indexOf(nueva_data);
+// El select, la tabla, el div (division en HTML para poder plasmar algo) y los sources llegan desde Python
+
+var label = cb_obj['label']; // Se fija el label del boton para mostrar ultimos eventos de la zona
+var nueva_data = select.value; // Se fija el valor del select para poder saber en que zona esta
+var indice = source.data.Intensity_tip.indexOf(nueva_data); // Busca el indice donde se encuentra dicha zona
 var j = 9;
-var data_source_historicos_muestra = source_historicos_muestra.data;
+
+var data_source_historicos_muestra = source_historicos_muestra.data; // Guarda la data del source que contiene los datos historicos para poder modificarlos luego
+
+// Vacia los arrays del source de historicos para que no haya conflictos al sobreescribir
 data_source_historicos_muestra.progre = [];
 data_source_historicos_muestra.id = [];
 data_source_historicos_muestra.vehi = [];
 data_source_historicos_muestra.fecha = [];
 data_source_historicos_muestra.estado = [];
+
+/* Se fija el valor del label del boton, si es para mostrar las zonas, busca los ultimos 10 eventos comprendidos entre las progresivas de la zona
+    y los asigna al source que utiliza la tabla, modifica la tabla, la muestra y modifica el boton en caso de querer ocultar los eventos.
+
+    En caso de que el label sea la segunda opcion, simplemente oculta la tabla con los ultimos eventos.*/
 if (label === 'Mostrar ultimos eventos de zona'){
     for (var i = source_eventos.data.progre.length - 1; i >= 0; i--){
         if (parseInt(source_eventos.data.progre[i]) >= parseInt(source.data.ProgresivaIni[indice].split('pk')[1]) && (source_eventos.data.progre[i] <= parseInt(source.data.ProgresivaFin[indice].split('pk')[1])) && j >= 0){
