cd4535df66730796de52629892d5e4bcac0d519f
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 09:08:01 -0300

    Agregado de comentarios

--- static/js/callbacks/guarda_zona.js
+++ static/js/callbacks/guarda_zona.js
@@ -1,5 +1,5 @@
-var f = cb_obj.value;
-var zona = source3.data.zona;
-zona[0] = f;
-source3.change.emit();
+var f = cb_obj.value; // Con cb_obj se obtiene el valor del select
+var zona = source3.data.zona; // Toma el valor de zona que esta en el source que llega desde Python
+zona[0] = f; // Le asigna el nuevo valor de zona tomado desde el select para poder usarlo despues
+source3.change.emit(); // Le avisa al source mediante esta linea que debe emitir un cambio
 console.log("callback completed");
