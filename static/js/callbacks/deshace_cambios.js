6504bf0b8da6ded6f4f92d6558c12029dd44a11e
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 11:28:34 -0300

    Agregado de comentarios

--- static/js/callbacks/deshace_cambios.js
+++ static/js/callbacks/deshace_cambios.js
@@ -1,14 +1,22 @@
-var s_data_cuadrados = source_cuadrados.data;
-var eve = [];
-var s_data_respaldo = source_respaldo.data;
-var s_data = source.data;
-var contNulls = 0;
+// Los datos de los sources, el slider, las herramientas y el select llegan desde Python por parametro
+
+// Todo el guardado de datos de los sources se realiza para poder trabajar mejor sobre ellos
+var s_data_cuadrados = source_cuadrados.data; // Guarda los datos del source de sombreado de zonas
+var eve = []; // Crea un vector que sirve para saber los eventos permitidos de la ultima zona modificada
+var s_data_respaldo = source_respaldo.data; // Guarda los datos del source de respaldo para poder volver a la ultima modificacion
+var s_data = source.data; // Guarda los datos del source de zonas original
+
+var contNulls = 0; // Crea un contador de zonas nulas, ya que la herramienta que modifica zonas a veces crea zonas nulas
+
+// Se fija cuantas zonas nulas hay y va sumando el contador
 for (var i = 0; i < s_data.Intensity_tip.length; i++){
     var veoUltimo = s_data.Intensity_tip[i];
     if (veoUltimo === null){
         contNulls++;
     }
 }
+
+// En caso de que el contador sea mayor a 0, elimina todas las zonas nulas
 if (contNulls > 0){
     for (var i = 0; i < contNulls; i++){
         s_data.Eventos_permitidos.pop();
@@ -23,6 +31,7 @@ if (contNulls > 0){
     }
 }
 else {
+    /* Caso contrario, recorre el source de respaldo para asignar los ultimos valores antes de la modificacion*/
     for (var i = 0; i < s_data_respaldo.length; i++){
         s_data.RT[i].fill(s_data_respaldo.RT[k][0]);
         s_data.RT_intensity.fill(s_data_respaldo.RT_intensity[k][0]);
@@ -37,6 +46,8 @@ else {
     s_data_respaldo.RT = s_data.RT.slice();
     s_data_respaldo.RT_intensity = s_data.RT_intensity.slice();
 }
+
+// Se fija que el source de sombreado de zonas tenga las coordenadas correctamente, caso contrario, las modifica
 var largo = s_data_cuadrados.x.length;
 for (var i = 0; i < largo; i++){
     s_data_cuadrados.x[i][0][0][2] = s_data.RT[i][0];
@@ -53,13 +64,18 @@ for (var i = 0; i < largo; i++){
         s_data_cuadrados.x[otro][0][0][1] = s_data.RT[i][0];
     }
 }
+
+// Se fija el indice de la ultima zona modificada y setea los eventos en el checkbox
 var indice = s_data.Intensity_tip.indexOf(source3.data.zona[0]);
 for (var k = 0; k < eventos_perm.length; k++){
     if (s_data.Eventos_permitidos[indice].includes(eventos_perm[k])){
         eve.push(k);
     }
 }
-poly_draw_tool.active = false;
+
+poly_draw_tool.active = false; // Desactiva la herramienta de modificacion de zonas
+
+// A su vez, crea una funcion range, ya que en JS no existe. La misma es utilizada para forzar a que la ultima zona sea inamovible y se mantenga estatica
 function range(start, end) {
     var ans = [];
     for (let i = start; i <= end; i++) {
@@ -67,6 +83,8 @@ function range(start, end) {
     }
     return ans;
 } 
+
+// Emite todos los cambios necesarios
 s_data.RT_intensity[s_data.RT_intensity.length - 1] = range(0, 49);
 eventos.active = eve;
 slider.value = s_data.rel_int[indice];
