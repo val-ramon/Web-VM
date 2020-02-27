8e264b7b77d52a766804bf6882d78aca6e61c74b
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 14:06:27 -0300

    Separación del main

--- src/flask_app.py
+++ src/flask_app.py
@@ -211,21 +211,3 @@ def map_zones():
         script, div = components(layout_b)
         return render_template("iris_index4.html", script=script, div=div,
                 feature_names=feature_names,  current_feature_name=current_feature_name)
-
-# With debug=True, Flask server will auto-reload 
-# when there are code changes
-    
-
-                                 
-if __name__ == '__main__':
-    """
-        Inicia el thread correspondiente para eliminar ip's en caso de inactividad, además de iniciar la app
-        de Flask.
-    """
-    thread_elimina_ips = threading.Thread(target=elimina_ips)
-    thread_elimina_ips.setDaemon(True)
-    thread_elimina_ips.start()
-    app.secret_key = os.urandom(12)
-    os.system("start cmd /k python mueve_archivo_cambios.py")
-    os.system("start cmd /k python crea_archivo_db.py")
-    app.run(port=5006, host='0.0.0.0')
