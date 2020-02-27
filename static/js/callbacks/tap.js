aabafe9ff78ea41d5a36b5f3517be449ba160f49
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 11:10:41 -0300

    Agregado de comentarios

--- static/js/callbacks/tap.js
+++ static/js/callbacks/tap.js
@@ -1,9 +1,14 @@
-var s_data = source.data;
-var x = cb_obj['x'];
-var eve = [];
-s_data_cuadrados = source_cuadrados.data;
+// Los datos de los sources, el select, el slider, el checkbox de eventos, los botones y las herramientas llegan por parametros desde Python
+
+var s_data = source.data; // Guarda la data del source de zonas en una variable
+var x = cb_obj['x']; // Como esta funcion detecta los clicks en la figura, se fija la coordenada en x para saber donde se hizo click
+var eve = []; // Crea un vector para poder agregar los eventos permitidos de la zona seleccionada 
+var s_data_cuadrados = source_cuadrados.data; // Guarda en una variable los datos del source de sombreado de zonas
+
+/* Primero se fija si la herramienta para modificar zonas esta activa, si no lo esta, la activa y se fija en que zona se hizo click,
+    esto es para darle al usuario una nocion visual de que zona esta modificando*/
 if (draw_tool_l1.active === false) {
-    draw_tool_l1.active = true;
+    draw_tool_l1.active = true; // Activa herramienta de modificacion de zonas
     for (var i = 0; i < s_data_cuadrados.color.length; i++){
         if (s_data.Eventos_permitidos[i].includes('Ninguno')){
             s_data_cuadrados.color[i] = 'White';
@@ -12,6 +17,10 @@ if (draw_tool_l1.active === false) {
             s_data_cuadrados.color[i] = 'Grey';
         }
     }
+
+    /* Recorre todo el source de las zonas para verificar que la coordenada en x coincida con alguna zona, una vez que la encuentra, la marca
+        y permite modificar la zona, mostrando la zona seleccionada con un color distinto, a su vez que modifica el select, lo bloquea y 
+        cambia la intensidad relativa y eventos permitidos a los de la zona*/
     var encontrado = false;
     for (var i = 0; i < s_data.RT.length; i++){
         if (i !== 0){
@@ -68,10 +77,12 @@ if (draw_tool_l1.active === false) {
             }
         }
     }
-    boton_muestra_eventos_zona.disabled = false;
-    boton_elimina_zona.disabled = false;
+    boton_muestra_eventos_zona.disabled = false; // Habilita el boton de mostrar ultimos eventos de zona
+    boton_elimina_zona.disabled = false; // Habilita el boton para eliminar zona, ya que hay una seleccionada
 }
 else{
+    /* En caso de que haya zonas seleccionadas y se vuelva a hacer click, deshabilita todos los botones y la tabla de ultimos eventos
+        asi como la herramienta para modificar zonas */
     boton_elimina_zona.disabled = true;
     boton_muestra_eventos_zona.disabled = true;
     div2.visible = false;
@@ -88,6 +99,8 @@ else{
     }
     select.disabled = false;
 }
+
+// Emite todos los cambios correspondientes en los sources
 source_respaldo.data = source.data;
 source_respaldo.change.emit();
 source.change.emit();
