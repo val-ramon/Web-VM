49a7ecda9285924737311f3d99972444af71b30d
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 09:39:38 -0300

    Agregado de comentarios

--- static/js/callbacks/filtra_fecha_fin.js
+++ static/js/callbacks/filtra_fecha_fin.js
@@ -1,2 +1,4 @@
+// Toma el valor del input ingresado donde se filtra por fecha final y se lo asigna al source de filtros que llega como parametro desde Python
+
 var fecha_fin = cb_obj['value_input'];
 source_filtros.data.rango_fecha[0][1] = fecha_fin;
