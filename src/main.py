e178ef999b178bb674f170e7d68f4a78f4104d61
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 14:07:14 -0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 14:04:34 2020
+
+@author: valramon
+"""
+
+from flask_app import *
+
+# With debug=True, Flask server will auto-reload 
+# when there are code changes                               
+if __name__ == '__main__':
+    """
+        Inicia el thread correspondiente para eliminar ip's en caso de inactividad, además de iniciar la app
+        de Flask.
+    """
+    thread_elimina_ips = threading.Thread(target=elimina_ips)
+    thread_elimina_ips.setDaemon(True)
+    thread_elimina_ips.start()
+    app.secret_key = os.urandom(12)
+    os.system("start cmd /k python mueve_archivo_cambios.py")
+    os.system("start cmd /k python crea_archivo_db.py")
+    app.run(port=5006, host='0.0.0.0')
