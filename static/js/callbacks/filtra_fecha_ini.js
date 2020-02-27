5c1bbfb669ac0dc185b22d3803ef34b2391c077a
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 09:38:41 -0300

    Agregado de comentarios

--- static/js/callbacks/filtra_fecha_ini.js
+++ static/js/callbacks/filtra_fecha_ini.js
@@ -1,2 +1,4 @@
+// Toma el valor del input ingresado donde se filtra por fecha inicial y se lo asigna al source de filtros que llega como parametro desde Python
+
 var fecha_ini = cb_obj['value_input'];
 source_filtros.data.rango_fecha[0][0] = fecha_ini;
