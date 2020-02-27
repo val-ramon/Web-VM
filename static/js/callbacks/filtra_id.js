08d4a995edeaa3943d8f780cf413e2ff8b28872c
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 10:47:15 -0300

    Agregado de comentarios

--- static/js/callbacks/filtra_id.js
+++ static/js/callbacks/filtra_id.js
@@ -1,12 +1,18 @@
-var id = cb_obj['value_input'];
-source_filtros.data.id[0] = id;
-data_table.visible = false;
+// Los sources y la tabla llegan desde Python pasados por parametros
+
+var id = cb_obj['value_input']; // Toma en tiempo real lo escrito en el input de filtrado de id y lo asigna a una variable
+source_filtros.data.id[0] = id; // Le asigna ese id al source de filtros para poder guardarlo
+data_table.visible = false; // Oculta momentaneamente la tabla para simular actualizacion de datos en tiempo real
+
+// Elimina todos los datos del source de historicos filtrados para evitar un conflicto al sobreescribir datos
 var j = 0;
 source_historicos_filtrado.data.id = [];
 source_historicos_filtrado.data.progre = [];
 source_historicos_filtrado.data.fecha = [];
 source_historicos_filtrado.data.vehi = [];
 source_historicos_filtrado.data.estado = [];
+
+// Recorre todo el source para ver cuales son los eventos que tienen el id correspondiente al filtrado, en caso de no haber ninguno, muestra todos
 for (var i = 0; i < source_historicos.data.progre.length; i++){
     if (source_historicos.data.id[i] === id){
         source_historicos_filtrado.data.id[j] = source_historicos.data.id[i];
@@ -25,5 +31,7 @@ for (var i = 0; i < source_historicos.data.progre.length; i++){
         j++;
     }
 }
+
+// Setea el nuevo source a la tabla y la muestra nuevamente
 data_table.source = source_historicos_filtrado;
 data_table.visible = true;
