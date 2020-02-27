14349d8d423235e42c8e2617a39fb2761c300e9e
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 09:27:04 -0300

    Agregado de comentarios

--- static/js/callbacks/mouse_move.js
+++ static/js/callbacks/mouse_move.js
@@ -1,11 +1,17 @@
+// El source llega como parametro desde Python
+
+// Crea una funcion range ya que en JS no existe dicha funcion
 function range(start, end) {
     var ans = [];
     for (let i = start; i <= end; i++) {
         ans.push(i);
     }
     return ans;
-} 
+}
+
+// Obliba al source a mantener sus coordenadas en y para que no se puedan ir las lineas de las zonas de la figura
 for (var j = 0; j < source.data.RT.length; j++){
     source.data.RT_intensity[j] = range(0, 49);
 }
-source.change.emit();
+
+source.change.emit(); // Aplica los cambios al source
