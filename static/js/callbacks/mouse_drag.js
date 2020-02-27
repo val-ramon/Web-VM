bffefafb027e13dbfd5ffd5362c92add4ba0601c
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 09:22:32 -0300

    Agregado de comentarios

--- static/js/callbacks/mouse_drag.js
+++ static/js/callbacks/mouse_drag.js
@@ -1,8 +1,14 @@
-var s_data = source.data;
-var s_data_cuadrados = source_cuadrados.data;
-var largo = s_data_cuadrados.x.length;
-var modifica = '';
-var eve = [];
+// Recibe los sources, el select y la herramienta mediante data pasada por parametro desde Python
+
+var s_data = source.data; // Asigna la data del source en una variable
+var s_data_cuadrados = source_cuadrados.data; // Asigna la data del source de sombreado a una variable
+var largo = s_data_cuadrados.x.length; // Se fija cuantas zonas hay
+var modifica = ''; // Crea una variable para saber la zona que debe dejar marcada y bloqueada en el select
+var eve = []; // Crea un array para poder agregar los eventos permitidos y poder marcarlos en el checkbox
+
+
+/* Recorre todas las zonas fijandose si hay algun cambio en las coordenadas en x de alguna zona y va modificando el sombreado en tiempo real,
+    a su vez que bloquea el select y muestra la zona seleccionada con otro color*/
 for (var i = 0; i < largo; i++){
     if (s_data_cuadrados.x[i][0][0][2] !== s_data.RT[i][0]){
         modifica = s_data.Intensity_tip[i];
@@ -25,6 +31,7 @@ for (var i = 0; i < largo; i++){
         s_data_cuadrados.x[otro][0][0][0] = s_data.RT[i][0];
         s_data_cuadrados.x[otro][0][0][1] = s_data.RT[i][0];
     }
+    // Este if - else sirve para modificar las progresivas en tiempo real
     if (i === 0){
         s_data.ProgresivaIni[i] = 'pk338';
     }
@@ -34,7 +41,10 @@ for (var i = 0; i < largo; i++){
     var suma = (s_data.RT[i][0] - 510)/1000 + 338;
     s_data.ProgresivaFin[i] = 'pk' + ((suma.toFixed(2).toString()));
 }
-s_data.RT[largo - 1].fill(largo_ducto);
+
+s_data.RT[largo - 1].fill(largo_ducto); // Rellena la ultima zona con la misma coordenada para forzar a que sea inamovible
+
+// Aplica los cambios a los sources
 source3.change.emit();
 source.change.emit();
 source_cuadrados.change.emit();
