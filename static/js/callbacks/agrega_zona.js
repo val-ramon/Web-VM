0c6be0d489a75507fc74f68b8d48b107770307dd
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-02-27 09:36:19 -0300

    Agregado de comentarios

--- static/js/callbacks/agrega_zona.js
+++ static/js/callbacks/agrega_zona.js
@@ -1,3 +1,8 @@
+// Todo lo utilizado aqui son elementos que llegan desde Python
+
+/* Se fija el valor del label del boton que se utiliza para agregar zonas, si esta en 'Agregar zonas', habilita los inputs para agregar las
+	progresivas y el boton para confirmar, en caso contrario, los deshabilita*/
+	
 if (cb_obj.label === 'Agregar zonas'){
     cb_obj.label = 'Terminar de agregar zonas';
     text_input_prog_ini.visible = true;
