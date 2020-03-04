8b793c6c2a620154102dfdd91ff88f6519050cd9
val-ramon <56452929+val-ramon@users.noreply.github.com>
2020-03-04 11:04:28 -0300

    Create mueve_archivo_db.py

---
+++ src/mueve_archivo_db.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 11:03:18 2020
+@author: valramon
+"""
+
+import os
+import shutil
+import glob
+import threading
+import time
+
+archVistos = []
+archNuevo = ''
+hayArchNuevo = False
+
+def miraArchivos(*args):
+    global archVistos, archNuevo, hayArchNuevo
+    while True:
+        try:
+            pathData = os.environ['USERPROFILE']+'/Downloads/'
+            os.chdir(pathData)
+            archivosCsv = glob.glob('db*.db')
+            archNuevo = ''
+            for archivo in archivosCsv:
+                if archivo not in archVistos:
+                    archNuevo = archivo
+                    archVistos.append(archivo)
+                    hayArchNuevo = True
+                    
+            if hayArchNuevo:
+                source = archNuevo
+                destination = os.environ['USERPROFILE']+'/Downloads/bokeh_flask/bokeh_flask_vm/' + archNuevo
+                shutil.copy(source, destination)
+                os.remove(source)
+                archVistos.remove(source)
+                hayArchNuevo = False
+        except:
+            pass
+        
+    
+miroArchNuevoThread = threading.Thread(target=miraArchivos)
+miroArchNuevoThread.setDaemon(True)
+miroArchNuevoThread.start()
+
+pathData = os.environ['USERPROFILE']+'/Downloads/'
+
+while True:
+    if hayArchNuevo:
+        time.sleep(1)
+        print('Archivo de base de datos movido.')
